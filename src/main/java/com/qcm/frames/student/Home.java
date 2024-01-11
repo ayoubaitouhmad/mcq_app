87e06a03033f2a33b203de9d90fc261d69bf3c9a
MRX <ayoubaithmad77@gmail.com>
2024-01-11 12:15:46 +0100

    add Singleton

--- src/main/java/com/qcm/frames/student/Home.java
+++ src/main/java/com/qcm/frames/student/Home.java
@@ -4,6 +4,7 @@ import main.java.com.qcm.DatabaseListExample;
 import main.java.com.qcm.model.Quiz;
 import main.java.com.qcm.model.Student;
 import main.java.com.qcm.panel.StudentPanel;
+import main.java.com.qcm.util.Sys;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,11 +16,32 @@ import java.util.List;
 
 public class Home extends JFrame {
 
+    private static Home instance;
+    private Student student;
+
     private DefaultListModel<String> listModel;
     private List<Quiz> items;
     private JList<String> itemList;
+    private List<Quiz> quizzes;
+    private JPanel mainPanel;
+
+    public static Home getInstance(Student student , boolean refresh ) throws SQLException {
+        if(refresh){
+            return  new Home(student);
+        }
+        if (instance == null) {
+            instance = new Home(student);
+        }
+        return instance;
+    }
+
+    public void refresh() throws SQLException {
+        fillData();
+    }
+
 
     public Home(Student student) throws SQLException {
+        this.student = student;
         setTitle("Styled Login and Navigation Example");
         setSize(700, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,26 +71,16 @@ public class Home extends JFrame {
         userInfoPanel.add(gradeValueLabel);
 
 
-        JPanel mainPanel = new JPanel(new BorderLayout());
+      mainPanel = new JPanel(new BorderLayout());
         mainPanel.add(userInfoPanel, BorderLayout.NORTH);
-        List<Quiz> quizzes = student.quizzes();
-        listModel = new DefaultListModel<>();
 
-        for (Quiz quiz: quizzes) {
-            listModel.addElement(quiz.getName());
-        }
-
-        itemList = new JList<>(listModel);
+        fillData();
 
 
-        itemList.addListSelectionListener(e -> {
-            if (!e.getValueIsAdjusting()) {
-                int selectedIndex = itemList.getSelectedIndex();
-                JOptionPane.showMessageDialog(Home.this, quizzes.get(selectedIndex).getId());
-            }
-        });
+//        itemList.setCellRenderer(new CustomBackgroundRenderer());
+//        changeBackgroundColor(itemList, 2, Color.RED);
 
-        mainPanel.add(new JScrollPane(itemList), BorderLayout.CENTER);
+
 
         // Set the main panel as the content pane
         setContentPane(mainPanel);
@@ -78,5 +90,70 @@ public class Home extends JFrame {
         setVisible(true);
     }
 
+
+    private  void fillData() throws SQLException {
+
+        quizzes = student.quizzes();
+        listModel = new DefaultListModel<>();
+
+        for (Quiz quiz : quizzes) {
+            listModel.addElement(quiz.getName(
+                    student
+            ));
+        }
+
+        itemList = new JList<>(listModel);
+        itemList.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                int selectedIndex = itemList.getSelectedIndex();
+                try {
+                    int score = student.quizScore(quizzes.get(selectedIndex).getId());
+                    System.out.println(student.quizScore(quizzes.get(selectedIndex).getId()));
+                    if (score == 0) {
+
+                        Home.getInstance(student,false).setVisible(false);
+                        new StartQuiz(
+                                quizzes.get(selectedIndex),
+                                student
+                        );
+
+                    } else {
+                        JOptionPane.showMessageDialog(Home.this, "Already take the quiz , score:" + score);
+                    }
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        });
+
+        mainPanel.add(new JScrollPane(itemList), BorderLayout.CENTER);
+    }
+
+    private static void changeBackgroundColor(JList<String> jList, int index, Color backgroundColor) {
+        DefaultListModel<String> model = (DefaultListModel<String>) jList.getModel();
+        model.getElementAt(index);
+        jList.repaint();
+    }
+
+    static class CustomBackgroundRenderer extends DefaultListCellRenderer {
+        @Override
+        public Component getListCellRendererComponent(JList<?> list, Object value, int index,
+                                                      boolean isSelected, boolean cellHasFocus) {
+            Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+
+            // Change background color for specific items
+            if (value != null && value.toString().endsWith("Item 2")) {
+                component.setBackground(Color.YELLOW);
+            } else if (value != null && value.toString().endsWith("Item 5")) {
+                component.setBackground(Color.GREEN);
+            } else {
+                // Reset background color for other items
+                component.setBackground(list.getBackground());
+            }
+
+            return component;
+        }
+    }
 
 }
