091a9bc84c4793b41918062deaa7d616d877f1fe
MRX <ayoubaithmad77@gmail.com>
2024-01-11 19:44:53 +0100

    add a new column to database

--- src/main/java/com/qcm/model/Professor.java
+++ src/main/java/com/qcm/model/Professor.java
@@ -11,19 +11,34 @@ public class Professor extends  Model {
 
     private int id;
     private String full_name;
+    private String cin;
     private String speciality;
 
 
 
-
+    public Professor() {}
 
     public Professor(int id, String full_name, String speciality) {
         this.id = id;
         this.full_name = full_name;
         this.speciality = speciality;
-
-
     }
+    public Professor(int id, String full_name, String speciality , String cin) {
+        this.id = id;
+        this.full_name = full_name;
+        this.speciality = speciality;
+        this.cin = cin;
+    }
+
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
 
     public int getId() {
         return id;
@@ -82,5 +97,66 @@ public class Professor extends  Model {
         }
         return null;
     }
+
+
+
+
+    public static Professor findByCin(String  cin) throws SQLException {
+        Connection connection = connection();
+        String query = "SELECT * FROM "+Professor.table+" WHERE cin = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, cin);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            return new Professor(
+                    resultSet.getInt("id"),
+                    resultSet.getString("full_name"),
+                    resultSet.getString("speciality"),
+                    resultSet.getString("cin")
+            );
+        }
+        return null;
+    }
+
+
+    /***
+     * create new Professor
+     * @return
+     * @throws SQLException
+     */
+    public Professor save() throws SQLException {
+        String insertQuery = "INSERT INTO " + Professor.table + " (full_name,speciality,cin) VALUES (?,?,?)";
+        String[] binding = {
+                this.getFull_name(),
+                this.getSpeciality(),
+                this.getCin()
+        };
+        PreparedStatement stm = query(insertQuery, binding);
+        stm.executeUpdate();
+        return  this;
+    }
+
+    /***
+     * Get First Student
+     * @return
+     * @throws SQLException
+     */
+    public static Professor first() throws SQLException {
+        String insertQuery = "select * from  " + Professor.table + " limit  1";
+        String[] binding = {};
+        PreparedStatement stm = query(insertQuery, binding);
+        stm.executeQuery();
+        ResultSet resultSet = stm.executeQuery();
+        if (resultSet.next()) {
+            return new Professor(
+                    resultSet.getInt("id"),
+                    resultSet.getString("full_name"),
+                    resultSet.getString("speciality"),
+                    resultSet.getString("cin")
+            );
+        }
+        return  null;
+    }
+
 
 }
